be0d185947f618329a79d3de830e1b88721099a2
Lucas Pereira <lucasp28.contato@gmail.com>
2020-12-08 16:44:38 -0300

    refactor: style disciplines page

--- src/app/main/disciplines/discipline-card/discipline-card.component.ts
+++ src/app/main/disciplines/discipline-card/discipline-card.component.ts
@@ -1,5 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Discipline } from 'src/app/interfaces/Discipline';
+import { 
+  faBrain, 
+  faChalkboardTeacher, 
+  faClipboard,
+  faGraduationCap,
+  faCalendarAlt 
+} from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-discipline-card',
@@ -7,8 +14,13 @@ import { Discipline } from 'src/app/interfaces/Discipline';
   styleUrls: ['./discipline-card.component.scss']
 })
 export class DisciplineCardComponent implements OnInit {
-  @Input() discipline: Discipline | undefined
-
+  @Input() discipline: Discipline | undefined;
+  faBrain = faBrain;
+  faChalkboardTeacher = faChalkboardTeacher;
+  faClipboard = faClipboard;
+  faGraduationCap = faGraduationCap;
+  faCalendarAlt = faCalendarAlt
+  
   constructor() { }
 
   ngOnInit(): void {
